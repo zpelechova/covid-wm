596840995a0f65092ee2da6ff0750ac12dd991a6
Zuzana Pelechová <39551460+zpelechova@users.noreply.github.com>
2020-04-30 23:21:28 +0200

    Changed datatype to numbers

--- main.js
+++ main.js
@@ -24,10 +24,11 @@ Apify.main(async () => {
 
         const regionsTableRows = Array.from(document.querySelectorAll("#main_table_countries_today > tbody > tr"));
         const regionData = [];
+        const toInt = (a) => Number(a.replace(',',''))
 
         for (const row of regionsTableRows) {
             const cells = Array.from(row.querySelectorAll("td")).map(td => td.textContent);
-            regionData.push({ country: cells[0], totalCases: cells[1], newCases: cells[2], totalDeaths: cells[3], newDeaths: cells[4], totalRecovered: cells[5], activeCases: cells[6], seriousCritical: cells[7], casesPerMil: cells[8], deathsPerMil: cells[9], totalTests: cells[10], testsPerMil: cells[11] });
+            regionData.push({ country: cells[0], totalCases: toInt(cells[1]), newCases: toInt(cells[2]), totalDeaths: toInt(cells[3]), newDeaths: toInt(cells[4]), totalRecovered: toInt(cells[5]), activeCases: toInt(cells[6]), seriousCritical: toInt(cells[7]), casesPerMil: toInt(cells[8]), deathsPerMil: toInt(cells[9]), totalTests: toInt(cells[10]), testsPerMil: toInt(cells[11]) });
         }
 
         const data = {
@@ -36,10 +37,10 @@ Apify.main(async () => {
         return data;
     });
     console.log(result)
-//    if (!result.country) {
-//        check = true;
-//    }
-//    else {
+    if (!result.country) {
+        check = true;
+    }
+    else {
         let latest = await kvStore.getValue(LATEST);
         if (!latest) {
             await kvStore.setValue('LATEST', result);
@@ -51,7 +52,7 @@ Apify.main(async () => {
 
         if (JSON.stringify(latest) !== JSON.stringify(actual)) {
             await dataset.pushData(result);
-  //      }
+        }
 
         await kvStore.setValue('LATEST', result);
         await Apify.pushData(result);
